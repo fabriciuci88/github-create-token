b7c25f37d6d038a24bb13c3f01a1a6d9f06642e0
kgryte <kgryte@gmail.com>
2016-03-04 19:00:34 -0800

    Add test fixtures, add request fcn, and add associated tests

---
+++ test/fixtures/opts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function getOpts() {
+	var opts = {
+		'token': 'abcdef',
+		'hostname': 'api.github.com',
+		'port': 443,
+		'protocol': 'https',
+		'scopes': ['public_repo'],
+		'note': 'beep'
+	};
+	return opts;
+}
+
+
+// EXPORTS //
+
+module.exports = getOpts;
